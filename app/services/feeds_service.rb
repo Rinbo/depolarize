4da2df848784dec198cf8ed43687e60ead25faff
rinbo <robin.b@outlook.com>
2018-10-03 16:52:37 +0200

    Add services folder and service model for API calls

---
+++ app/services/feeds_service.rb
@@ -0,0 +1,17 @@
+require 'open-uri'
+require 'json'
+
+module FeedsService  
+
+  def self.get_feed    
+    url = "https://newsapi.org/v2/everything?"\
+      "q=Politics&"\
+      "from=2018-10-03&"\
+      "sortBy=popularity&"\
+      "apiKey=#{Rails.application.credentials.news[:api_key]}"
+    req = open(url)
+    response = JSON.parse(req.read)
+    response["articles"]
+  end
+
+end
