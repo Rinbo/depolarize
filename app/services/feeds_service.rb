f822c66f7d555c8bc5ef7af534e7e668f20813d5
danmalmx <danmalmx@gmail.com>
2018-10-04 09:22:50 +0200

    refactoring some tailwind css

--- app/services/feeds_service.rb
+++ app/services/feeds_service.rb
@@ -6,7 +6,7 @@ module FeedsService
   def self.get_feed    
     url = "https://newsapi.org/v2/everything?"\
       "q=Politics&"\
-      "from=2018-10-03&"\
+      "from=2018-10-04&"\
       "sortBy=popularity&"\
       "apiKey=#{Rails.application.credentials.news[:api_key]}"
     req = open(url)
