f39efcb91a32946831552f3a8938328f9c209920
rinbo <robin.b@outlook.com>
2018-10-05 11:08:31 +0200

    Remove temporary switch variable from feeds controller

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,7 +1,5 @@
-class FeedsController < ApplicationController
-  @@arr = (1..100).to_a
-  def index
-    @switch = @@arr.pop 
+class FeedsController < ApplicationController  
+  def index    
     @liberal_feed = FeedsService.get_feed('opinion', 'usa-today', 'usatoday.com')
     @conservative_feed = FeedsService.get_feed('opinion', 'the-new-york-times', 'nytimes.com')    
   end
