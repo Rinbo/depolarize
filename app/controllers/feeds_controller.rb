8100e8d1de7134e4714aa5c0cc653e147bede0c8
rinbo <robin.b@outlook.com>
2018-10-05 11:15:16 +0200

    Add step in welcom controller to redirect to feeds path if logged in

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,6 @@
-class FeedsController < ApplicationController  
-  def index    
+class FeedsController < ApplicationController
+  before_action :authenticate_user!
+  def index   
     @liberal_feed = FeedsService.get_feed('opinion', 'usa-today', 'usatoday.com')
     @conservative_feed = FeedsService.get_feed('opinion', 'the-new-york-times', 'nytimes.com')    
   end
