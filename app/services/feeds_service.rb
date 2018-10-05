ee91d9316082d7c3c7c91cfe550947a87c95581c
rinbo <robin.b@outlook.com>
2018-10-05 09:31:14 +0200

    Remove commented out code

--- app/services/feeds_service.rb
+++ app/services/feeds_service.rb
@@ -14,13 +14,3 @@ module FeedsService
     
   end
 end
-
-# all_articles = 
-# url = "#{BASE_URI}/everything?"\
-#       "q=Politics&"\
-#       "from=2018-10-04&"\
-#       "sortBy=popularity&"\
-#       "apiKey=#{Rails.application.credentials.news[:api_key]}"
-#     req = open(url)
-#     response = JSON.parse(req.read)
-#     response["articles"]
