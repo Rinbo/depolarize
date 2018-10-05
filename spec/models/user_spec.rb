41202e726bef0f31ed19df7436c18e9d91948494
rinbo <robin.b@outlook.com>
2018-10-05 10:37:02 +0200

    Add migration with affiliation column to user and modify unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_db_column :id }
   it { is_expected.to have_db_column :email }
   it { is_expected.to have_db_column :encrypted_password }
+  it { is_expected.to have_db_column :affiliation }
 end
