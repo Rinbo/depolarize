efc23b6dc6493a131aa0d49143a8bb8c444a4472
rinbo <robin.b@outlook.com>
2018-10-05 10:57:03 +0200

    Generate registrations controller with devise and modify routes to point to new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations' }
   root controller: :welcome, action: :index
   resources :feeds,  only: [:index]
 end
