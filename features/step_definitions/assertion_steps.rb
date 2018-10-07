264d306282e4787bb75042e1078b28341f130f78
Robin Borjesson <robin.b@outlook.com>
2018-10-07 12:33:34 +0200

    Add facebook_signup feature and corresponding step definitions

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -10,3 +10,7 @@ end
 Then("I am on the login page") do
   expect(current_path).to eq user_session_path
 end
+
+Then("I should be redirected to the news feed") do
+  expect(current_path).to eq feeds_path
+end
