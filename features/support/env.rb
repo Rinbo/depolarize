ed098e6d76ad5a782a0d965c7576fd790c33c6b9
Robin Borjesson <robin.b@outlook.com>
2018-10-07 14:40:40 +0200

    Add facebook secret key and app key and config setup in devise.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,12 @@
 require 'cucumber/rails'
 
 ActionController::Base.allow_rescue = false
+
+Before do 
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(OmniAuthFixtures.facebook_mock)
+end
+
 begin
   DatabaseCleaner.strategy = :transaction
 rescue NameError
